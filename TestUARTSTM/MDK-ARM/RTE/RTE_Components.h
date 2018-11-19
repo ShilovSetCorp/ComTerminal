650f372399c2d6faa452646bd5f04236961f9aae
roman <shilov-rom@mail.ru>
2018-11-19 17:32:50 +0300

    added project for nucleo plate and libs jssc

---
+++ TestUARTSTM/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TestUARTSTM' 
+ * Target:  'TestUARTSTM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
